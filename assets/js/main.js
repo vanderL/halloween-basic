f98ed84aa2c841af3943b3dd1c41ce384aaf8d8a
Vander Lima <vnd.vander@gmail.com>
2023-03-09 00:47:51 -0300

    feat: Scroll Sections Active Link

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,14 +30,32 @@ navLink.forEach(item => item.addEventListener('click', linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById('header');
-  console.log('foi')
   this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
 }
 
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/
 
